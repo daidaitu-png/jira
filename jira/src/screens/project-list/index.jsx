c1ca22827c1c17b95dd1dc1fc30c60135fded882
liuming <15926724726@163.com>
2022-02-25 08:19:37 +0800

    useDebounce

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -1,6 +1,9 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
+import { cleanObj, useMount,useDebounce } from "../../utils";
+import * as qs from "qs";
+
 const apiUrl = process.env.REACT_APP_URL;
 
 export const ProjectListScreen = () => {
@@ -11,20 +14,33 @@ export const ProjectListScreen = () => {
     name: "",
     personId: "",
   });
+
+  const debouncedParam = useDebounce(param,2000)
+
   useEffect(() => {
-    fetch(`${apiUrl}/projects`).then(async (response) => {
+    fetch(
+      // `${apiUrl}/projects?name=${param.name}&personId=${param.personId}`
+      `${apiUrl}/projects?${qs.stringify(cleanObj(debouncedParam))}`
+    ).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
-  useEffect(() => {
+  }, [debouncedParam]);
+  // useEffect(() => {
+  //   fetch(`${apiUrl}/users`).then(async (response) => {
+  //     if (response.ok) {
+  //       setUsers(await response.json());
+  //     }
+  //   });
+  // }, []);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
