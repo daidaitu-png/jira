ad1f0db398f7e3de70614ff609faca32b9d725b6
liuming <15926724726@163.com>
2022-02-28 08:16:27 +0800

    用Row布局页面

--- jira/src/authenticated-app.tsx
+++ jira/src/authenticated-app.tsx
@@ -1,10 +1,58 @@
+import styled from "@emotion/styled"
+import { Row } from "components/lib"
 import { useAuth } from "context/auth-context"
 import { ProjectListScreen } from "screens/project-list"
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
-  return <div>
-    <button onClick={logout}> logout </button>
-    <ProjectListScreen />
-  </div>
+  return <Container>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h3>Logo</h3>
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <button onClick={logout}> logout </button>
+      </HeaderRight>
+    </Header>
+    <Main>
+      <ProjectListScreen />
+    </Main>
+  </Container>
 }
+
+const PageHeader = styled.header`
+  height: 6rem;
+`
+
+const Main = styled.main`
+  height: calc(100vh-6rem);
+`
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`
+
+const Header = styled(Row)`
+  /* display: flex;
+  flex-direction: row;
+  align-items: center; */
+  /* justify-content: space-between; */
+`
+
+const HeaderLeft = styled(Row)`
+  /* display: flex;
+  align-items: center; */
+`
+
+const HeaderRight = styled.div`
+  
+`
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`
+
